133e249b091c088d0e452afd23eb0c78a07f24cf
Alberto Rodriguez <ardlema@gmail.com>
2013-11-03 10:37:39 +0100

    PASO 7 - Test 4 tiradas

--- src/test/scala/JuegoTests.scala
+++ src/test/scala/JuegoTests.scala
@@ -24,12 +24,40 @@ class JuegoTests extends FunSuite {
     assert(juegoConDosLanzamientos.getPuntuacion() == puntuacionTotal,
       s"El marcador despues de los lanzamientos no es $puntuacionTotal")
   }
+
+  test("añadir cuatro lanzamientos sin tirar todos los bolos en ninguno de ellos") {
+    val puntuacionPrimerLanzamiento = 5
+    val puntuacionSegundoLanzamiento = 4
+    val puntuacionTercerLanzamiento = 2
+    val puntuacionCuartoLanzamiento = 6
+    val puntuacionTotalPrimeraJugada = puntuacionPrimerLanzamiento + puntuacionSegundoLanzamiento
+    val puntuacionTotalSegundaJugada = puntuacionTercerLanzamiento + puntuacionCuartoLanzamiento
+    val puntuacionTotal = puntuacionTotalPrimeraJugada + puntuacionTotalSegundaJugada
+
+    val juegoConCuatroLanzamientos = new Juego
+
+    juegoConCuatroLanzamientos.añadir(puntuacionPrimerLanzamiento)
+    juegoConCuatroLanzamientos.añadir(puntuacionSegundoLanzamiento)
+    juegoConCuatroLanzamientos.añadir(puntuacionTercerLanzamiento)
+    juegoConCuatroLanzamientos.añadir(puntuacionCuartoLanzamiento)
+
+    assert(juegoConCuatroLanzamientos.getPuntuacion() == puntuacionTotal,
+      s"El marcador total despues de los lanzamientos no es $puntuacionTotal")
+
+    assert(juegoConCuatroLanzamientos.getPuntuacionParaJugada(1) == puntuacionTotalPrimeraJugada,
+      s"El marcador para la primera jugada no es $puntuacionTotalPrimeraJugada")
+
+    assert(juegoConCuatroLanzamientos.getPuntuacionParaJugada(2) == puntuacionTotalSegundaJugada,
+      s"El marcador para la segunda jugada no es $puntuacionTotalSegundaJugada")
+  }
 }
 
 class Juego {
   var puntuacion = 0
 
   def getPuntuacion() = puntuacion
+
+  def getPuntuacionParaJugada(jugada: Int) = 0
 
   def añadir(lanzamiento: Int) {
     puntuacion += lanzamiento
