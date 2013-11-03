82fe2c181266ae73271d44caf90550758fdb3066
Alberto Rodriguez <ardlema@gmail.com>
2013-11-03 09:58:23 +0100

    PASO 3 - Creación de test para marcador con lanzamiento y metodo para añadir lanzamiento

--- src/test/scala/MarcadorTests.scala
+++ src/test/scala/MarcadorTests.scala
@@ -10,10 +10,23 @@ class MarcadorTests extends FunSuite {
 
     assert(marcadorSinLanzamiento.getPuntuacion() == marcadorInicial, s"El marcador inicial no es $marcadorInicial")
   }
+
+  test("puntuación del marcador con un lanzamiento") {
+    val puntuacionDelLanzamiento = 5
+    val marcadorConLanzamiento = new Marcador
+
+    marcadorConLanzamiento.añadirLanzamiento(puntuacionDelLanzamiento)
+
+    assert(marcadorConLanzamiento.getPuntuacion() == puntuacionDelLanzamiento, s"El marcador tras el lanzamiento no es $puntuacionDelLanzamiento")
+  }
 }
 
 class Marcador {
   def getPuntuacion() = 0
+
+  def añadirLanzamiento(lanzamiento: Lanzamiento) {
+
+  }
 }
 
 
